1772405a94fdb8d35fca73ff67693c9ac0b27b76
Hiroaki Ninomiya <me@treby.info>
2017-11-19 02:16:59 +0900

    imasquare host to environment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :unicorn_rack_env, 'production'
 
-role :app, %w(ec2-52-197-19-202.ap-northeast-1.compute.amazonaws.com)
-server 'ec2-52-197-19-202.ap-northeast-1.compute.amazonaws.com', user: 'ec2-user', roles: %w(app)
+role :app, [ENV['IMASQUARE_HOST']]
+server ENV['IMASQUARE_HOST'], user: 'ec2-user', roles: %w(app)
 
 set :ssh_options, {
   keys: %w(/Users/treby/.ssh/imasquare.pem),
